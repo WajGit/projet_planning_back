66f237fbfcf897e9dafe58cdede29feaa8053a70
WajGit <w.karoui@gmail.com>
2025-05-08 16:12:47 +0200

    Etape 7 - Modif DayType + Controller

--- src/Controller/DayTypeController.php
+++ src/Controller/DayTypeController.php
@@ -6,6 +6,7 @@ use App\Entity\WeekType;
 use App\Entity\DayType;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,28 +18,29 @@ use Symfony\Component\Routing\Attribute\Route;
 final class DayTypeController extends AbstractController
 {
     #[Route('/add', name: 'api_day_add', methods: ['POST'])]
-    public function add(Request $request, EntityManagerInterface $entityManager): JsonResponse
-    {
+    public function add(Request $request, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse {
         $data = json_decode($request->getContent(), true);
         $user = $this->getUser();
         if (!$user) return new JsonResponse(['error' => 'Non autorisé'], 401);
-
+    
         if (empty($data['name']) || empty($data['weekId'])) {
             return new JsonResponse(['error' => 'Nom ou semaine manquant'], 400);
         }
-
         $week = $entityManager->getRepository(WeekType::class)->find($data['weekId']);
         if (!$week) return new JsonResponse(['error' => 'Semaine introuvable'], 404);
-
         $day = new DayType();
         $day->setName($data['name']);
         $day->setWeek($week);
-
         $entityManager->persist($day);
         $entityManager->flush();
-
-        return new JsonResponse(['message' => 'Jour ajouté', 'dayId' => $day->getId()], 201);
+        return new JsonResponse([
+            'message' => 'Jour ajouté',
+            'weekId' => $day->getWeek()->getId(),
+            'dayId' => $day->getId(),
+            'dayName' => $day->getName(),
+        ], 201);
     }
+    
 
     #[Route('/{id}', name: 'api_day_update', methods: ['PUT'])]
     public function update(int $id, Request $request, EntityManagerInterface $entityManager): JsonResponse
